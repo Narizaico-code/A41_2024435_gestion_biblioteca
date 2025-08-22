3f6ec73e06157a033186cf66b7551f373129a348
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2025-08-22 15:45:06 -0600

    Gestion de biblioteca primer commit

---
+++ src/test/java/org/jrae/gestion_biblioteca/GestionBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrae.gestion_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
