916f0bc8e2c3f0373f06b146cc4dc45f7cd79b27
Informatica <informatica@alumno.local>
2025-08-29 15:25:56 -0600

    Hola

---
+++ src/main/java/org/jrae/gestion_biblioteca/GestionBibliotecaApplicationWeb.java
@@ -0,0 +1,11 @@
+package org.jrae.gestion_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibliotecaApplicationWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBibliotecaApplication.class, args);
+    }
+}
---
+++ src/main/java/org/jrae/gestion_biblioteca/web/controller/AutoresController.java
@@ -0,0 +1,4 @@
+package org.jrae.gestion_biblioteca.web.controller;
+
+public class AutoresController {
+}
